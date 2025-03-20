5fff294e7e057468063d398a11b41680d06adaee
baoyuan zeng <zeng.bao@northeastern.edu>
2025-03-20 19:04:05 -0400

    edit Player.java for basic attribute and getter setter func

--- Player.java
+++ Player.java
@@ -1,2 +1,41 @@
+/**
+ * The class represents the Player Class.
+ */
 public class Player {
+  public String name; // no exits in Player class in UML - need discussion
+  public Integer score;
+  public Integer health;
+  public Integer capacity;
+  public Room currentRoom;
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getScore() {
+    return score;
+  }
+
+  public void setScore(Integer score) {
+    this.score = score;
+  }
+
+  public Integer getHealth() {
+    return health;
+  }
+
+  public Integer getCapacity() {
+    return capacity;
+  }
+  
+  public Room getCurrentRoom() {
+    return currentRoom;
+  }
+
+
 }
