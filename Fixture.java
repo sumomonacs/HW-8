017ca03a7b2d7e78d81ed2cef13b243ba8f488ce
zengby-aa <zeng.bao@northeastern.edu>
2025-03-20 17:24:52 -0400

    Add files via upload
    Edit Fixture class

--- Fixture.java
+++ Fixture.java
@@ -6,4 +6,52 @@ public class Fixture {
   public String states;
   public String description;
   public String picture;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getWeight() {
+    return weight;
+  }
+
+  public void setWeight(int weight) {
+    this.weight = weight;
+  }
+
+  public String getPuzzle() {
+    return puzzle;
+  }
+
+  public void setPuzzle(String puzzle) {
+    this.puzzle = puzzle;
+  }
+
+  public String getStates() {
+    return states;
+  }
+
+  public void setStates(String states) {
+    this.states = states;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getPicture() {
+    return picture;
+  }
+
+  public void setPicture(String picture) {
+    this.picture = picture;
+  }
 }
