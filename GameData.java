003a6e0f7c31675c6ae6ecf57a57b411077b6203
LZC173 <chen.liangz@northeastern.edu>
2025-03-24 07:41:46 +0800

    Add files via upload

--- GameData.java
+++ GameData.java
@@ -1,7 +1,7 @@
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
-
+// game data package eveything except
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GameData {
     public String name;
@@ -9,5 +9,6 @@ public class GameData {
     public List<Item> items;
     public List<Monster> monsters;
     public List<Puzzle> puzzles;
+    public List<Fixture> fixtures;
 
 }
