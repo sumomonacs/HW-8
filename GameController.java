67a40dfc25add5047f7457ac3ddf58989ce704c7
LZC173 <chen.liangz@northeastern.edu>
2025-03-24 08:33:59 +0800

    Job distribution

---
+++ GameController.java
@@ -0,0 +1,21 @@
+
+
+public class GameController {
+  private Map map;
+  private Player player;
+
+
+  //move method -- Yijie Li
+
+  // look method and examine --Alexender
+
+  //take and drop item --Amy
+
+  // use item and anwser question to solve monster and puzzle-- David Liu,
+
+  // save and load game -- Chen
+
+
+  // Dostring- Abdullahi Abdirahman:
+
+}
