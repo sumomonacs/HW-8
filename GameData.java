df8d1e9c701f151f0ca851009f51d29a532a6569
LZC173 <chen.liangz@northeastern.edu>
2025-03-27 09:36:22 +0800

    data format changes

--- GameData.java
+++ GameData.java
@@ -11,4 +11,51 @@ public class GameData {
     public List<Puzzle> puzzles;
     public List<Fixture> fixtures;
 
+    public String getName() {
+        return name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public List<Monster> getMonsters() {
+        return monsters;
+    }
+
+    public List<Puzzle> getPuzzles() {
+        return puzzles;
+    }
+
+    public List<Fixture> getFixtures() {
+        return fixtures;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public void setMonsters(List<Monster> monsters) {
+        this.monsters = monsters;
+    }
+
+    public void setPuzzles(List<Puzzle> puzzles) {
+        this.puzzles = puzzles;
+    }
+
+    public void setFixtures(List<Fixture> fixtures) {
+        this.fixtures = fixtures;
+    }
 }
--- Map.java
+++ Map.java
@@ -4,16 +4,36 @@ import java.util.List;
 // a map class that contains a list of room
 public class Map {
   private List<Room> rooms;
+  private String name;
+  private String version;
 
-
-  public Map(List<Room> rooms) {
+  public Map(List<Room> rooms, String name, String version) {
     this.rooms = rooms;
+    this.name = name;
+    this.version = version;
   }
+
   public List<Room> getRooms() {
     return rooms;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getVersion() {
+    return version;
+  }
+
   public void setRooms(List<Room> rooms) {
     this.rooms = rooms;
   }
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setVersion(String version) {
+    this.version = version;
+  }
 }
